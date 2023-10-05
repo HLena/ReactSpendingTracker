a377d211de5e289106c986caa934b1671ac9c413
HLena <gordillo.vi20@gmail.com>
2023-10-05 16:10:06 -0500

    Add styles for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="bg-teal-500 text-white font-extrabold uppercase text-center h-3/6">
+        <h1 className="py-12 text-2xl">Planificador de gastos</h1>
+    </header>
+  )
+}
+
+export default Header
