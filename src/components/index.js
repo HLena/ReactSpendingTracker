d058afc5237f38ad075cac7e35b578964aac5cf0
HLena <gordillo.vi20@gmail.com>
2023-10-16 22:51:30 -0500

    chrore: Add Expense and ExpenseList component

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Header from './Header'
+import NewBudget from './NewBudget'
+import Message from './Message'
+import ControlBudget from './ControlBudget'
+import Modal from './Modal';
+import Expense from './Expense';
+import ExpenseList from './ExpenseList';
+
+
+export {
+    Header,
+    NewBudget,
+    Message,
+    ControlBudget,
+    Modal,
+    Expense,
+    ExpenseList
+}
