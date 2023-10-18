84ae2a7800a53a247cd97807d2e3eb0417a5329a
HLena <gordillo.vi20@gmail.com>
2023-10-17 19:26:46 -0500

    styles: Add margin

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,5 +1,5 @@
 
-const ControlBudget = ({budget}) => {
+const ControlBudget = ({budget, totalExpenses}) => {
 
     const formatQuantity = (quantity) => {
         return quantity.toLocaleString('en-US',{
@@ -12,9 +12,9 @@ const ControlBudget = ({budget}) => {
         <div className="absolute -bottom-5 p-4 bg-white rounded-md shadow-md w-11/12 md:w-1/2 grid grid-cols-2 gap-2">
             <div>Grafica</div>
             <div className="text-left">
-                <p><span className="text-teal-500 font-bold">Presupuesto:{' '}</span>{formatQuantity(budget)}</p>
-                <p><span className="text-teal-500 font-bold">Disponible:{' '}</span>{formatQuantity(budget)}</p>
-                <p><span className="text-teal-500 font-bold">Gastado:{' '}</span>{formatQuantity(budget)}</p>
+                <p className="my-2"><span className="text-teal-500 font-bold">Presupuesto:{' '}</span>{formatQuantity(budget)}</p>
+                <p className="my-2"><span className="text-teal-500 font-bold">Disponible:{' '}</span>{formatQuantity(budget - totalExpenses)}</p>
+                <p className="my-2"><span className="text-teal-500 font-bold">Gastado:{' '}</span>{formatQuantity(totalExpenses)}</p>
             </div>
         </div>
     )
