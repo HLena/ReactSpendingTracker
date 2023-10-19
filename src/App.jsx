bf04e33b1560eafd42d947bf4b4e28d9c5cb6ba6
HLena <gordillo.vi20@gmail.com>
2023-10-18 20:54:11 -0500

    feat: set and get the budget value and the expenses array in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,64 @@ import { useEffect, useState } from "react";
 import { ExpenseList, Header, Modal }from "./components/index";
 import iconNewbudget from './img/nuevo-gasto.svg';
 import { generateId } from "./helpers";
+import Filter from "./components/Filter";
 
 function App() {
 
-  const [budget, setBudget] = useState('');
-  const [totalExpenses, setTotalExpenses] = useState(0);
-  const [isValidBudget, setIsValidBudget] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [expenses, setExpenses] = useState([]);
+  const [budget, setBudget] = useState(Number(localStorage.getItem('budget')) ?? 0);
 
-  const closeModal = () => setIsModalOpen(false);
+  const [expenses, setExpenses] = useState(JSON.parse(localStorage.getItem('expenses')) || []);
+
+  const [isValidBudget, setIsValidBudget] = useState(false);
+
+  const [totalExpenses, setTotalExpenses] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editExpense, setEditExpense] = useState(null);
+  const [deleteExpense, setDeleteExpense] = useState(null);
+  const [porcent, setPorcent] = useState(0);
+  const [filter, setFilter] = useState('');
+
+  const closeModal = () =>{ 
+    setIsModalOpen(false);
+    setEditExpense(null)
+  }
+  
   const openModal = () => setIsModalOpen(true);
 
   useEffect(() => {
-    const total = expenses.reduce((total, expense) => total + Number(expense.quantity), 0);
-    setTotalExpenses(total);
-    
+    const budgetLS = Number(localStorage.getItem('budget')) ?? 0;
+    if(budgetLS > 0) setIsValidBudget(true);
+  }, [])
+  
+
+  useEffect(() => {
+    localStorage.setItem('budget', JSON.stringify(budget) ?? 0);
+  }, [budget])
+
+  useEffect(() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses) ?? []);
+  }, [expenses])
+  
+
+  useEffect(() => {
+    if(expenses?.length > 0) {
+      const total = expenses.reduce((total, expense) => total + Number(expense.quantity), 0);
+      const newPorcent = ((total / budget) * 100).toFixed(2);
+      setTotalExpenses(total);
+      setPorcent(Number(newPorcent))
+    }
   }, [expenses]);
 
-  const addExpense = (newExpense) => {
+  useEffect(() => {
+    if(editExpense){
+      openModal()
+    } else if(deleteExpense) {
+      onDeleteExpense()
+    }
+   
+  },[editExpense, deleteExpense])
+
+  const onAddExpense = (newExpense) => {
     setExpenses([
       ...expenses,
       {
@@ -31,6 +70,16 @@ function App() {
     ])
   }
 
+  const onEditExpense = (values) => {
+    setExpenses(expenses.map(expense => expense.id == values.id ? values : expense))
+    setEditExpense(null)
+  }
+
+  const onDeleteExpense = () => {
+    setExpenses(expenses.filter(expense => expense.id != deleteExpense ))
+    setDeleteExpense(null)
+  }
+
   return (
     <>
       <Header 
@@ -39,12 +88,20 @@ function App() {
         isValidBudget = {isValidBudget}
         setIsValidBudget = { setIsValidBudget }
         totalExpenses = {totalExpenses}
+        porcent = {porcent}
+        setExpenses={ setExpenses }
       />
       {
           isValidBudget && (
             <>
-              <main className="mt-8 mx-auto w-11/12 md:w-1/2 ">
-                <ExpenseList expenses = {expenses}/>
+              <main className="mt-8 mx-auto w-11/12 sm:w-3/4 max-w-md">
+                <Filter setFilter = {setFilter} filter = {filter}/>
+                <ExpenseList 
+                  expenses = {expenses} 
+                  setEditExpense = {setEditExpense}
+                  setDeleteExpense = {setDeleteExpense}
+                  filter = {filter}
+                />
               </main>
               <div className="p-2 w-14 fixed right-0 bottom-0 hover: cursor-pointer" onClick={openModal}>
                 <img src={iconNewbudget} alt="" />
@@ -56,9 +113,11 @@ function App() {
         isModalOpen && 
         <Modal 
           onClose={closeModal} 
-          onSaveExpense = {addExpense} 
+          onAddExpense = {onAddExpense} 
+          onEditExpense = {onEditExpense} 
           totalExpenses = { totalExpenses }
           budget = { budget }
+          editExpense = {editExpense}
         />
       }
     </>
